a9fcfee3e57250a42eb690202a1360d014d01d4a
Alex Licata <alerandom99@gmail.com>
2020-03-18 12:42:58 +0100

    Updates

---
+++ precache-manifest.6ead6aeac5c28278e9008cce6c90a44e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4ca5b01a41eefe8f1c6e3a294be4375",
+    "url": "/body-girl-animal-client/index.html"
+  },
+  {
+    "revision": "282f0aa7b01842c5a4ee",
+    "url": "/body-girl-animal-client/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "6b35db9adb75764916c1",
+    "url": "/body-girl-animal-client/static/css/main.fa37ff0a.chunk.css"
+  },
+  {
+    "revision": "282f0aa7b01842c5a4ee",
+    "url": "/body-girl-animal-client/static/js/2.2d8f6277.chunk.js"
+  },
+  {
+    "revision": "36185af7fd86c208509c0f419d435d95",
+    "url": "/body-girl-animal-client/static/js/2.2d8f6277.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b35db9adb75764916c1",
+    "url": "/body-girl-animal-client/static/js/main.05495e16.chunk.js"
+  },
+  {
+    "revision": "a33fb9a971d73990531a",
+    "url": "/body-girl-animal-client/static/js/runtime-main.6fd51d2c.js"
+  }
+]);
