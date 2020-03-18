10ca1a981af24e9fd1d75485b5a508bc95d06eac
Alex Licata <alerandom99@gmail.com>
2020-03-18 13:49:05 +0100

    Updates

--- precache-manifest.7ace6b63c940d8b73cedc03b86f889d7.js
+++ precache-manifest.7ace6b63c940d8b73cedc03b86f889d7.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5f9dabf5fd4a7f517d0d896ad2e5c8bb",
+    "revision": "974417dcd46fe60f27fc338f83480167",
     "url": "/body-girl-animal-client/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/body-girl-animal-client/static/css/2.de424728.chunk.css"
   },
   {
-    "revision": "9b06a351f03d965ebba1",
+    "revision": "e994840e1cc2ff32a080",
     "url": "/body-girl-animal-client/static/css/main.fa37ff0a.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/body-girl-animal-client/static/js/2.2d8f6277.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "9b06a351f03d965ebba1",
-    "url": "/body-girl-animal-client/static/js/main.89c44654.chunk.js"
+    "revision": "e994840e1cc2ff32a080",
+    "url": "/body-girl-animal-client/static/js/main.9412dd0f.chunk.js"
   },
   {
     "revision": "a33fb9a971d73990531a",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/body-girl-animal-client/precache-manifest.93abb28236e1207a429ee1eb940f0285.js"
+  "/body-girl-animal-client/precache-manifest.7ace6b63c940d8b73cedc03b86f889d7.js"
 );
 
 self.addEventListener('message', (event) => {
